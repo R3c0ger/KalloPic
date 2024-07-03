16007c60a9d9a028205ee1cfba8c30c3552e367d
Recogerous <2737936634@qq.com>
2024-07-03 23:23:15 +0800

    adjust project structure and add constants class
    - Change src to kallopic.
    - Add Constants class.
    - Add author, banner and github url.

---
+++ kallopic/constants.py
@@ -0,0 +1,19 @@
+#!usr/bin/env python
+# -*- coding: utf-8 -*-
+
+class Constants:
+    PROJECT_NAME = "KalloPic"
+    PROJECT_NAME_LOWER = PROJECT_NAME.lower()
+    VERSION = "0.1.0"
+    AUTHOR = "Recogeta"
+    DESCRIPTION = "Folder-based picture viewer, filter and archiver."
+    URL = 'https://github.com/R3c0ger/KalloPic'
+    BUG_REPORT_URL = 'https://github.com/R3c0ger/KalloPic/issues'
+    BANNER = """
+    ██╗░░██╗░█████╗░██╗░░░░░██╗░░░░░░█████╗░██████╗░██╗░█████╗░
+    ██║░██╔╝██╔══██╗██║░░░░░██║░░░░░██╔══██╗██╔══██╗██║██╔══██╗
+    █████═╝░███████║██║░░░░░██║░░░░░██║░░██║██████╔╝██║██║░░╚═╝
+    ██╔═██╗░██╔══██║██║░░░░░██║░░░░░██║░░██║██╔═══╝░██║██║░░██╗
+    ██║░╚██╗██║░░██║███████╗███████╗╚█████╔╝██║░░░░░██║╚█████╔╝
+    ╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝╚══════╝░╚════╝░╚═╝░░░░░╚═╝░╚════╝░
+    """  # thanks to https://text-symbols.com/
